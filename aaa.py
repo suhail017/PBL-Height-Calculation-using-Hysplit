87eaf1c519b68f60bcc567949efb21c78c1677f6
Suhail <suhail017@gmail.com>
2018-09-07 02:57:06 -0600

    Add files via upload

---
+++ aaa.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("mix_hgts_sort.txt") as f:
+	
+	lines = f.readlines()[1:]
+	
+
+x = [line.split()[1] for line in lines]
+y = [line.split()[3] for line in lines]
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("PBLH")    
+ax1.set_xlabel('hour')
+ax1.set_ylabel('mixing hgt')
+
+ax1.plot(x,y, c='r')
+
+leg = ax1.legend()
+
+plt.show()
